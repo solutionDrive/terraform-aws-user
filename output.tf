b9df019e22cb302e53250063aa31f44776dbff70
Matthias Alt <alt@solutiondrive.de>
2018-03-02 14:29:42 +0100

    added needed resources variables and outputs for user creation

---
+++ output.tf
@@ -0,0 +1,15 @@
+output "unique_id" {
+  value = "${aws_iam_user.user.unique_id}"
+}
+
+output "arn" {
+  value = "${aws_iam_user.user.arn}"
+}
+
+output "user_name" {
+  value = "${aws_iam_user.user.name}"
+}
+
+output "password" {
+  value = "${aws_iam_user_login_profile.user.encrypted_password}"
+}
